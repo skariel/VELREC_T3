39f2b2b5c3295696d790495f84ad79e6ed7beb57
Ariel Keselman <skariel@gmail.com>
2016-02-09 11:26:29 +0200

    better realization

--- jl/dark_sky_halos_1600.jl
+++ jl/dark_sky_halos_1600.jl
@@ -21,14 +21,20 @@ immutable Halo1600
     pid::Int64
 end
 
-function load_raw_halos1600(minimalmass=20.0, substructure=false)
+function load_raw_halos1600(minimalmass=10.0, substructure=false)
     # translate mass to DS units...
     minimalmass *= 1e10
 
     const N = 3000000
     const HEADER_SIZE = 1216 + (20+4)*64
     tmp_halos = Array(Halo1600, N)
-    chalos = Halo1600[]
+    _x = Float32[]
+    _y = Float32[]
+    _z = Float32[]
+    _vx = Float32[]
+    _vy = Float32[]
+    _vz = Float32[]
+    _m = Float32[]
     const sz = stat(RAW_DS_FN).size - HEADER_SIZE
     open(RAW_DS_FN, "r") do f
         # skip the header and sha1...
@@ -45,11 +51,16 @@ function load_raw_halos1600(minimalmass=20.0, substructure=false)
                 !substructure && h.pid >= 0 && continue
                 h.m200b < minimalmass && continue
                 tot += 1
-                push!(chalos, h)
+                push!(_x, h.x)
+                push!(_y, h.y)
+                push!(_z, h.z)
+                push!(_vx, h.vx)
+                push!(_vy, h.vy)
+                push!(_vz, h.vz)
+                push!(_m, h.m200b)
             end
-            println("percentage read: ",(position(f)-HEADER_SIZE)/sz*100.0," fraction surviving mass filter: ",tot/N," halos#: ",length(chalos))
+            println("percentage read: ",(position(f)-HEADER_SIZE)/sz*100.0," fraction surviving mass filter: ",tot/N," halos#: ",length(_x))
         end
-
         # eat whatever remains
         tmp_halos2 = Array(Halo1600, round(Int, (sz-position(f))/sizeof(Halo1600)))
         # read halos
@@ -60,53 +71,21 @@ function load_raw_halos1600(minimalmass=20.0, substructure=false)
             for h in tmp_halos2
                 !substructure && h.pid >= 0 && continue
                 h.m200b < minimalmass && continue
-                push!(chalos, h)
+                push!(_x, h.x)
+                push!(_y, h.y)
+                push!(_z, h.z)
+                push!(_vx, h.vx)
+                push!(_vy, h.vy)
+                push!(_vz, h.vz)
+                push!(_m, h.m200b)
             end
             println(position(f)/sz*100.0)
         end
     end
     # translate units to G2
-    @inbounds for i in eachindex(chalos)
-        chalos[i] = Halo1600(
-            chalos[i].x*1000.0,
-            chalos[i].y*1000.0,
-            chalos[i].z*1000.0,
-            chalos[i].vx,
-            chalos[i].vy,
-            chalos[i].vz,
-            chalos[i].mvir/1e10,
-            chalos[i].m200b/1e10,
-            chalos[i].m200c/1e10,
-            chalos[i].m500c/1e10,
-            chalos[i].m2500c/1e10,
-            chalos[i].vmax,
-            chalos[i].rvmax,
-            chalos[i].r200b*1000.0,
-            chalos[i].spin,
-            chalos[i].kin_to_pot,
-            chalos[i].id,
-            chalos[i].pid
-        )
-    end
-    chalos
-end
-
-function get_pos_vel_mass(halos::Array{Halo1600,1})
-    x = zeros(Float32, length(halos))
-    y = zeros(Float32, length(halos))
-    z = zeros(Float32, length(halos))
-    vx = zeros(Float32, length(halos))
-    vy = zeros(Float32, length(halos))
-    vz = zeros(Float32, length(halos))
-    m = zeros(Float32, length(halos))
-    @inbounds for i in eachindex(halos)
-        x[i] = halos[i].x
-        y[i] = halos[i].y
-        z[i] = halos[i].z
-        vx[i] = halos[i].vx
-        vy[i] = halos[i].vy
-        vz[i] = halos[i].vz
-        m[i] = halos[i].m200b
-    end
-    x,y,z,vx,vy,vz,m
+    _x *= 1000.0
+    _y *= 1000.0
+    _z *= 1000.0
+    _m /= 1f10
+    _x, _y, _z, _vx, _vy, _vz, _m
 end
