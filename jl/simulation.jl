d6ae31e79f7d5416e24cabfd5baea8dd46f22060
Ariel Keselman <skariel@gmail.com>
2016-02-17 09:44:34 +0200

    added 2lpt simulation and optimization. Starting to test

--- jl/simulation.jl
+++ jl/simulation.jl
@@ -10,3 +10,22 @@ function simulate_zeld!(rho, pos, m, a_from, a_to, side_len=SIDE_LEN)
     end
     info("simzel end")
 end
+
+function simulate_2lpt!(rho, pos, m, a_from, a_to, fac2=1.0, side_len=SIDE_LEN)
+    info("sim2lpt start from a=",a_from," to a=",a_to)
+    to_rho!(pos,m, rho);
+    rho_to_1st_order_vel_pot!(rho);
+    fac1 = 1.0
+    for dim in 1:3
+        info("sim2lpt 1st order dim ",dim)
+        get_1st_order_s!(c, a_from, a_to, dim, rho)
+        move_periodic!(pos, dim, c, fac1, side_len)
+    end
+    first_order_vel_pot_to_sencond_order!(rho)
+    for dim in 1:3
+        info("sim2lpt 2nd order dim ",dim)
+        get_2nd_order_s!(c, a_from, a_to, dim, rho)
+        move_periodic!(pos, dim, c, fac2, side_len)
+    end
+    info("sim2lpt end")
+end
