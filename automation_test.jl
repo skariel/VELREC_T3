0829c0c2a641995d97251433230b12ad6e676103
Ariel Keselman <skariel@gmail.com>
2016-02-22 15:34:18 +0200

    some automation

---
+++ automation_test.jl
@@ -0,0 +1,9 @@
+const REALIZATION_TYPE = "small"
+const PROCS_NUM        = 8
+const MASS_REAL        = true
+const OPT_TYPE         = "fwd"
+const SIM_TYPE         = "zel"
+const ICS              = "target"
+const LOGGING_FOLDER   = "AUTOMATION_TEST"
+
+include("jl/init_by_params.jl")
